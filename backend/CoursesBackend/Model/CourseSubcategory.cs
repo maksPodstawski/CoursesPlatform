b8de1ae77a4b09b5f9bb0a9c02c6d85118fa7c82
maksPodstawski <m.podstawski34@gmail.com>
2025-04-10 19:26:08 +0200

    changing model

---
+++ backend/CoursesBackend/Model/CourseSubcategory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CourseSubcategory
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid CourseId { get; set; }
+
+        [ForeignKey(nameof(CourseId))]
+        public Course Course { get; set; }
+
+        public Guid SubcategoryId { get; set; }
+        [ForeignKey(nameof(SubcategoryId))]
+        public Subcategory Subcategory { get; set; }
+
+
+    }
+}
