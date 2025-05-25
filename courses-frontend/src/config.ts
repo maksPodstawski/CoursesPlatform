6a278eda6f174fd729cb2edb3d3ec3164f80fa19
Mateusz Guzy <guzymateusz02@gmail.com>
2025-05-25 19:13:25 +0200

    apiBaseUrl variable

--- courses-frontend/src/config.ts
+++ courses-frontend/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-	apiBaseUrl: import.meta.env.API_BASE_URL || "https://localhost:7207",
+	apiBaseUrl: import.meta.env.API_BASE_URL || "https://api.czester.ovh/",
 	apiEndpoints: {
 		login: "/api/account/login",
 		register: "/api/account/register",
